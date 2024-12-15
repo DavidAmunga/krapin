9d103bcff5be5dd40a60f9d9491d82ba2a25eb78
David Amunga <david.onyimbo@gmail.com>
2024-12-15 14:00:39 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <head>
+        <title>KRA PIN</title>
+        <meta
+          name="description"
+          content="Store all your KRA PINs in one place"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <main>
+        <h1 className="text-2xl text-red-600 font-bold">KRA PIN</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
